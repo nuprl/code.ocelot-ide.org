2dc9e115a5d3d42dcc01cd956284476804d490f8
Arjun Guha <arjun@cs.umass.edu>
2019-07-21 13:55:54 -0400

    Update

--- dist/store/notification/types.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// ---------- ACTION TYPES ----------
-exports.TRIGGER_NOTIFICATION = 'TRIGGER_NOTIFICATION';
-exports.CLOSE_NOTIFICATION = 'CLOSE_NOTIFICATION';
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidHlwZXMuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi8uLi8uLi9zcmMvc3RvcmUvbm90aWZpY2F0aW9uL3R5cGVzLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBRUEscUNBQXFDO0FBQ3hCLFFBQUEsb0JBQW9CLEdBQUcsc0JBQXNCLENBQUM7QUFDOUMsUUFBQSxrQkFBa0IsR0FBRyxvQkFBb0IsQ0FBQyJ9
