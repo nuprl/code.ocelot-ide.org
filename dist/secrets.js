c9cacb68e62f459b3a339062a19f454ead8b13c0
Arjun Guha <a.guha@northeastern.edu>
2021-05-13 10:04:10 -0400

    x

--- dist/secrets.js
+++ dist/secrets.js
@@ -1,6 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.MODULE_WL_URL = exports.LOGIN_CLIENT_ID = exports.CLD_FN_BASE_URL = void 0;
-const isProd = window.location.hostname === 'www.ocelot-ide.org';
-exports.CLD_FN_BASE_URL = `https://us-central1-arjunguha-research-group.cloudfunctions.net/ocelot/`, exports.LOGIN_CLIENT_ID = '692270598994-p92ku4bbjkvcddouh578eb1a07s8mghc.apps.googleusercontent.com', exports.MODULE_WL_URL = `https://raw.githubusercontent.com/umass-compsci220/ocelot-settings/master/libraries${isProd ? '' : '-beta'}.json`;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic2VjcmV0cy5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9zZWNyZXRzLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7OztBQUFBLE1BQU0sTUFBTSxHQUFZLE1BQU0sQ0FBQyxRQUFRLENBQUMsUUFBUSxLQUFLLG9CQUFvQixDQUFDO0FBQzdELFFBQUEsZUFBZSxHQUFXLHlFQUF5RSxFQUNuRyxRQUFBLGVBQWUsR0FBVywwRUFBMEUsRUFDcEcsUUFBQSxhQUFhLEdBQVcsc0ZBQXNGLE1BQU0sQ0FBQyxDQUFDLENBQUMsRUFBRSxDQUFDLENBQUMsQ0FBQyxPQUFPLE9BQU8sQ0FBQyJ9
+const isProd = true;
+// The URL to the Cloud Function on the backend
+exports.CLD_FN_BASE_URL = `https://us-central1-ocelot-ide-org.cloudfunctions.net/ocelot/`;
+// From backend/env.yaml
+exports.LOGIN_CLIENT_ID = '842833214550-7apj11arna9fauh5tba8hl3bnej3dhqc.apps.googleusercontent.com';
+exports.MODULE_WL_URL = `https://raw.githubusercontent.com/umass-compsci220/ocelot-settings/master/libraries${isProd ? '' : '-beta'}.json`;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic2VjcmV0cy5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9zZWNyZXRzLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7OztBQUFBLE1BQU0sTUFBTSxHQUFHLElBQUksQ0FBQztBQUNwQiwrQ0FBK0M7QUFDbEMsUUFBQSxlQUFlLEdBQUcsK0RBQStELENBQUM7QUFDL0Ysd0JBQXdCO0FBQ1gsUUFBQSxlQUFlLEdBQUcsMEVBQTBFLENBQUM7QUFDN0YsUUFBQSxhQUFhLEdBQUcsc0ZBQXNGLE1BQU0sQ0FBQyxDQUFDLENBQUMsRUFBRSxDQUFDLENBQUMsQ0FBQyxPQUFPLE9BQU8sQ0FBQyJ9
